e00b87d4bd1950ed851f185068937a551bdaa1fa
asdcloud <asdcloud01@gmail.com>
2022-05-28 18:06:44 +0800

    Player script change

---
+++ testproject/Assets/script/P1_Movement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1_Movement : MonoBehaviour {
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    void Update() {
+        horizontalMove = Input.GetAxisRaw("P1_Horizontal") * runSpeed;
+        if (Input.GetButtonDown("P1_Jump")) {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate() {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
--- testproject/Assets/script/P2_Movement.cs
+++ testproject/Assets/script/P2_Movement.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class P2_Movement : MonoBehaviour {
     public CharacterController2D controller;
     public float runSpeed = 40f;
     float horizontalMove = 0f;
     bool jump = false;
 
     void Update() {
-        horizontalMove = Input.GetAxisRaw("P1_Horizontal") * runSpeed;
-        if (Input.GetButtonDown("P1_Jump")) {
+        horizontalMove = Input.GetAxisRaw("P2_Horizontal") * runSpeed;
+        if (Input.GetButtonDown("P2_Jump")) {
             jump = true;
         }
     }
