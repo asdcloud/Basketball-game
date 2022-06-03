eb91f53f0f3507b615ca06bd52194b3cb5f8a2db
asdcloud <asdcloud01@gmail.com>
2022-06-03 19:33:23 +0800

    test hand

---
+++ testproject/Assets/script/testhand.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testhand : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float speed;
+    public float jumpForce;
+    private float jumpTimeCounter;
+    public float jumpTime;
+    private bool isJumping;
+
+    private float moveInput;
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate() {
+        moveInput = Input.GetAxisRaw("P2_Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+    void Update() {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        if(isGrounded == true && Input.GetButtonDown("P2_Jump")) {
+            isJumping = true;
+            jumpTimeCounter = jumpTime; 
+            rb.velocity = Vector2.up * jumpForce;
+        }
+        if(Input.GetButton("P2_Jump") && isJumping == true) {
+            if(jumpTimeCounter > 0) {
+                rb.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            } else {
+                isJumping = false;
+            }
+        }
+        if(Input.GetButtonUp("P2_Jump")) {
+            isJumping = false;
+        }
+    }
+}
