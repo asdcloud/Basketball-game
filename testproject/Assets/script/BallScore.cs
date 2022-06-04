dcf50b6c74ebdf3fa1d609e4817c492e5e4222f2
410410030 <101887340+410410030@users.noreply.github.com>
2022-06-04 18:12:16 +0800

    ballscore

---
+++ testproject/Assets/script/BallScore.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        Vector2 transPos = transform.position;
+        Vector2 closePos = other.ClosestPoint(transPos);
+        Vector2 dir = (transPos-closePos).normalized;
+        if(other.tag=="scorearea1"&& dir.y>=1)
+        {
+            Debug.Log("1");
+        }
+        else if(other.tag=="scorearea2"&& dir.y>=1)
+        {
+            Debug.Log("2");
+        }
+    }
+}
