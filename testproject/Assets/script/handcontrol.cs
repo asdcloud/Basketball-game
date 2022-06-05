35f0db8bbfdc63c46cca84faecc9685279c696a2
asdcloud <asdcloud01@gmail.com>
2022-06-05 22:48:05 +0800

    player hand

--- testproject/Assets/script/handcontrol.cs
+++ testproject/Assets/script/handcontrol.cs
@@ -8,7 +8,7 @@ public class handcontrol : MonoBehaviour
     [SerializeField] public float handSpeed = 0.2f;
     private float handInput;
     public CollisionDetectionMode collisionDetectionMode;
-
+    private int Trigger = 0;
     public Rigidbody2D rigidBody2D;
     public void AddTorqueImpulse(float angularChangeInDegrees)
     {
@@ -26,15 +26,19 @@ public class handcontrol : MonoBehaviour
     {
         handInput = Input.GetAxisRaw("Hand"); 
         //按鈕是否被按下s
-        if(handInput == 1)
+        if(handInput == 1 && Trigger == 0)
         {
             AddTorqueImpulse(handSpeed);
             //rigidBody2D.rotation += handSpeed;
             //transform.Rotate(new Vector3(0, 0, handSpeed));
-        }else if (handInput == -1) {
+            Trigger = 1;
+        }else if (handInput == -1 && Trigger == 0) {
             AddTorqueImpulse(handSpeed * handInput);
             //rigidBody2D.rotation += handSpeed * handInput;
             //transform.Rotate(new Vector3(0, 0, handSpeed * handInput));
+            Trigger = 1;
+        }else if (handInput == 0) {
+            Trigger = 0;
         }
     }
 }
