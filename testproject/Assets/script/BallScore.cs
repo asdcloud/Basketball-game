32eb44c273d47ae86d4c74cc901dbab29db64e1e
guantou0817 <guantou@alum.ccu.edu.tw>
2022-06-07 15:45:43 +0800

    SCORE

--- testproject/Assets/script/BallScore.cs
+++ testproject/Assets/script/BallScore.cs
@@ -2,36 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+
 
 public class BallScore : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
+  
+
+
     void Start()
     {
         
+      
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
     }
 
-    void OnTriggerEnter2D(Collider2D other) 
+    void OnTriggerEnter2D(Collider2D other)
     {
+       
         Vector2 transPos = transform.position;
         Vector2 closePos = other.ClosestPoint(transPos);
-        Vector2 dir = (transPos-closePos).normalized;
-        if(other.tag=="scorearea1"&& dir.y>=1)
+        Vector2 dir = (transPos - closePos).normalized;
+        if (other.tag == "scorearea1" && dir.y >= 1)
         {
             Debug.Log("1");
             Scorecode.Score1 = Scorecode.Score1 + 1;
         }
-        else if(other.tag=="scorearea2"&& dir.y>=1)
+        else if (other.tag == "scorearea2" && dir.y >= 1)
         {
+            Scorecode.Score2 = Scorecode.Score2 + 1;
             Debug.Log("2");
-            
+                
         }
     }
 }
--- testproject/Assets/script/Scorecode.cs
+++ testproject/Assets/script/Scorecode.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Scoreboard : MonoBehaviour
 {
     public static int Score1;
-    public Text ShowScore1;
+    public TextMeshProUGUI ShowScore1;
+    public TextMeshProUGUI ShowScore2;
     public static int Score2;
-    public Text ShowScore2;
+    
 
     // Start is called before the first frame update
     void Start()
