32647c89fec87b96f362f98777be647fc1241bbb
Freddy9940 <106302102+Freddy9940@users.noreply.github.com>
2022-06-08 15:21:33 +0800

    重新發球 修正

--- testproject/Assets/script/BallScore.cs
+++ testproject/Assets/script/BallScore.cs
@@ -11,6 +11,8 @@ public class BallScore : MonoBehaviour
     public Transform player2Pos;
     public Transform basketballPos;
     public Rigidbody2D basketballRigidbody;
+    public Rigidbody2D player1Rigidbody;
+    public Rigidbody2D player2Rigidbody;
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -23,16 +25,18 @@ public class BallScore : MonoBehaviour
             Debug.Log("1");
             Scorecode.Score1 = Scorecode.Score1 + 1;
             basketballPos.position = new Vector2(-6, 36);
-            player1Pos.position = new Vector2(3, 25);
+            player1Pos.position = new Vector2(3, 27);
             basketballRigidbody.velocity = new Vector2(0, 5);
+            player1Rigidbody.velocity = new Vector2(0, 5);
         }
         else if (other.tag == "scorearea2" && dir.y >= 1)
         {
             Scorecode.Score2 = Scorecode.Score2 + 1;
             Debug.Log("2");
             basketballPos.position = new Vector2(3, 36);   
-            player2Pos.position = new Vector2(-6, 25);
+            player2Pos.position = new Vector2(-6, 27);
             basketballRigidbody.velocity = new Vector2(0, 5);
+            player2Rigidbody.velocity = new Vector2(0, 5);
         }
         
         if (Scorecode.Score1 >= 3)
