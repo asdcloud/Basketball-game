6295b8437f2771f5ece408decd97de68b9c4c190
guantou0817 <guantou@alum.ccu.edu.tw>
2022-06-10 17:28:56 +0800

    bgm 音量修改

---
+++ testproject/Assets/script/volumecontrol.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class volumecontrol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private AudioSource audioSrc;
+    public Slider musicSlider;
+    // Music volume variable that will be modified
+    // by dragging slider knob
+    private float musicVolume = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        // Assign Audio Source component to control it
+        audioSrc = GetComponent<AudioSource>();
+        musicSlider.value = PlayerPrefs.GetFloat("Music", 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Setting volume option of Audio Source to be equal to musicVolume
+        audioSrc.volume = musicVolume;
+    }
+
+    // Method that is called by slider game object
+    // This method takes vol value passed by slider
+    // and sets it as musicValue
+    public void SetVolume(float vol)
+    {
+        PlayerPrefs.SetFloat("Music", vol);
+        musicVolume = PlayerPrefs.GetFloat("Music");
+    }
+}
